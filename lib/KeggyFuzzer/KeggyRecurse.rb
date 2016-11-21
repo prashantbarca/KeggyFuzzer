d466cfd458c97b3bd7dd9dfddab0e7dedeaa5447
Prashant <prashant.barca@gmail.com>
2016-11-20 23:42:07 -0500

    Implement basic fuzzer generator

--- lib/KeggyFuzzer/KeggyRecurse.rb
+++ lib/KeggyFuzzer/KeggyRecurse.rb
@@ -1,69 +1,87 @@
 module KeggyFuzzer
+  #
   class KeggyRecurse
 
-    # The static method that gets called from outside. Should verify if it is the Hammer::Parser method.
-    def self.caller(args)
+    private
+
+    # hammer.many - 0 or more occurences of whatever is given
+    def many(args)
+      poss = []
+      if args.class == String
+        (0..10).to_a.each do |i|
+          poss.push(args * i)
+        end
+      elsif args.class == Array
+        args.each do |arg|
+          (0..10).to_a.each do |i|
+            poss.push(arg * i)
+          end
+        end
+      end
+      poss
+    end
+    
+    # hammer.many1 - 1 or more occurences of whatever is given
+    def many1(args)
+      poss = []
+      if args.class == String
+        (1..10).to_a.each do |i|
+          poss.push(args * i)
+        end
+      elsif args.class == Array
+        args.each do |arg|
+          (1..10).to_a.each do |i|
+            poss.push(arg * i)
+          end
+        end
+      end
+      poss
+    end
+
+    # hammer.middle
+    def middle(args)
+    end
+    
+    # The static method that gets called from outside.
+    # Should verify if it is the Hammer::Parser method.
+
+    public
+
+    def caller(args)
       # puts args.inspect
       # only sequence, in and not_in return lists
       if args.name == :sequence
         array = []
-        for i in args.dont_gc
+        args.dont_gc.each do |i|
           array.push(caller(i))
         end
-        return_string = {:sequence => array}
-        return return_string
+        poss = array[0]
+        (1..array.length - 1).to_a.each do |j|
+          current_pos = poss.map { |x| array[j].map { |y| x + y } }.flatten
+          poss = current_pos
+        end
+        poss
       elsif args.name == :token
-        return_string = {:token => args.dont_gc[1]}
-        return return_string
+        [args.dont_gc[1]]
       elsif args.name == :action
-        return caller(args.dont_gc.first)
+        caller(args.dont_gc.first)
       elsif args.name == :ch
-        return {:ch => args.dont_gc.first}
+        args.dont_gc.first
       elsif args.name == :many
-        return {:many => caller(args.dont_gc.first)}
+        many(caller(args.dont_gc.first))
+      elsif args.name == :many1
+        many1(caller(args.dont_gc.first))
       elsif args.name == :ch_range
-        return {:ch_range => [args.dont_gc.first, args.dont_gc.last]}
+        poss = []
+        (args.dont_gc.first..args.dont_gc.last).each do |i|
+          poss.push(i.chr)
+        end
+        poss
       elsif args.name == :in
-        return {:in => args.dont_gc.first.split('')}
+        args.dont_gc.first.split('')
       elsif args.name == :end_p
-        return {:end_p => nil}
+        ['']
       end
     end
-
-    # hammer.action
-    private def action(args)
-            end
-
-    # hammer.sequence - join more than 1 string
-    private def sequence(args)
-            end
-
-    # hammer.token
-    private def token(args)
-            end
-
-    # hammer.many - 0 or more occurences of whatever is given
-    private def many(args)
-            end
-
-    # hammer.many1 - 1 or more occurences of whatever is given
-    private def many1(args)
-              
-            end
-
-    # hammer.ch - 1 character
-    private def ch(args)
-              
-            end
-
-    # hammer.ch_range - 1 character in the range
-    private def ch_range(args)
-              
-            end
-
-    # hammer.in - choices
-    private def in(args)
-            end
-
   end
 end
